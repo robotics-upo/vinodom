d7af960e330b5bff72f6b97aaef218da42c77ae7
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-01 11:45:58 +0200

    Revert "Merge branch 'master' of github.com:aerostack2-developers/vinodom"
    This reverts commit 3501f86e0254b41fe0da58b58821312ff5711baa, reversing
    changes made to d47fed3d13b83cb3f8a48ca31fd84147cfa2901e.

--- launch/vinodom_launch.py
+++ launch/vinodom_launch.py
@@ -6,16 +6,12 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument('namespace', default_value='quadrotor_1'),
+        DeclareLaunchArgument('namespace', default_value='quadrotor_3'),
         DeclareLaunchArgument('camera_topic', default_value='slot3'),
         DeclareLaunchArgument('imu_topic', default_value='imu/data'),
-        DeclareLaunchArgument(
-            'odom_topic', default_value='visual_odometry/odom'),
         DeclareLaunchArgument('altimeter_topic', default_value='slot6/scan'),
-        DeclareLaunchArgument('barometer_topic', default_value='air_pressure'),
-        DeclareLaunchArgument('min_plane_dist', default_value='5.0'),
-        DeclareLaunchArgument('base_frame', default_value='quadrotor_1'),
-        DeclareLaunchArgument('show_matching', default_value='false'),
+        DeclareLaunchArgument('base_frame', default_value='quadrotor_3'),
+        DeclareLaunchArgument('show_matching', default_value='true'),
         Node(
             package='vinodom',
             executable='vinodom',
@@ -23,16 +19,11 @@ def generate_launch_description():
             parameters=[{'namespace ': LaunchConfiguration('namespace')},
                         {'camera_topic': LaunchConfiguration('camera_topic')},
                         {'imu_topic': LaunchConfiguration('imu_topic')},
-                        {'odom_topic': LaunchConfiguration('odom_topic')},
-                        {'altimeter_topic': LaunchConfiguration(
-                            'altimeter_topic')},
-                        {'barometer_topic': LaunchConfiguration(
-                            'barometer_topic')},
-                        {'min_plane_dist': LaunchConfiguration(
-                            'min_plane_dist')},
+                        {'altimeter_topic': LaunchConfiguration('altimeter_topic')},
                         {'base_frame': LaunchConfiguration('base_frame')},
                         {'show_matching': LaunchConfiguration('show_matching')}],
             output='screen',
             emulate_tty=True
         )
     ])
+
