edd79ef9e94d40e7536b9728809dba4ac52fbbc9
perezsaura-david <perezsaura.david@gmail.com>
2022-08-01 13:38:56 +0200

    Add new launch parameters

--- launch/vinodom_launch.py
+++ launch/vinodom_launch.py
@@ -14,6 +14,11 @@ def generate_launch_description():
         DeclareLaunchArgument('altimeter_topic', default_value='slot6/scan'),
         DeclareLaunchArgument('barometer_topic', default_value='air_pressure'),
         DeclareLaunchArgument('min_plane_dist', default_value='5.0'),
+        DeclareLaunchArgument("init_x", default_value='0.0'),
+        DeclareLaunchArgument("init_y", default_value='0.0'),
+        DeclareLaunchArgument("init_z", default_value='0.0'),
+        DeclareLaunchArgument("override_height_with_bar",
+                              default_value='true'),
         DeclareLaunchArgument('base_frame', default_value='quadrotor_1'),
         DeclareLaunchArgument('show_matching', default_value='false'),
         Node(
@@ -30,6 +35,11 @@ def generate_launch_description():
                             'barometer_topic')},
                         {'min_plane_dist': LaunchConfiguration(
                             'min_plane_dist')},
+                        {'init_x': LaunchConfiguration('init_x')},
+                        {'init_y': LaunchConfiguration('init_y')},
+                        {'init_z': LaunchConfiguration('init_z')},
+                        {'override_height_with_bar': LaunchConfiguration(
+                            'override_height_with_bar')},
                         {'base_frame': LaunchConfiguration('base_frame')},
                         {'show_matching': LaunchConfiguration('show_matching')}],
             output='screen',
